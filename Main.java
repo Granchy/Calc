30fb94190b83771c0f06ef9c489e17b3f824cb7a
Granchy <125912971+Granchy@users.noreply.github.com>
2023-08-12 23:16:23 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,6 +7,8 @@ import javax.swing.*;
 public class Main {
 
     public static void main(String[] args) {
+//        double x = Double.parseDouble("80.0")/100.0;
+//        System.out.println(x);
         SwingUtilities.invokeLater(() -> {
             GUI gui = new GUI();
         });
